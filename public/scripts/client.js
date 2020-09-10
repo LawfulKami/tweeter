8388b1f6c050dc72eb9ba0606b51621b5c8a85a7
Lanny Rivest <lanny.rivest@gmail.com>
2020-09-09 20:18:48 -0400

    Adds action buttons layout, no functionalties.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -31,7 +31,14 @@ const createTweetElement = function(tweet) {
   <img class="small-avatar" src="${avatars}"><p>${name}</p><span class="handle">${handle}</span>
   </header>
   <p>${escape(text)}</p>
-  <footer>10 days ago</footer>
+  <footer class="tweetfooter">
+  <span>10 days ago</span>
+  <div>
+  <button class="icon flag"><svg class="actions" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-flag"><path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"></path><line x1="4" y1="22" x2="4" y2="15"></line></svg></button>
+  <button class="icon retweet"><svg class="actions" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-repeat"><polyline points="17 1 21 5 17 9"></polyline><path d="M3 11V9a4 4 0 0 1 4-4h14"></path><polyline points="7 23 3 19 7 15"></polyline><path d="M21 13v2a4 4 0 0 1-4 4H3"></path></svg></button>
+  <button class="icon like"><svg class="actions" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path></svg></button>
+  </div>
+  </footer>
   </article>`;
   return $tweet;
 };
@@ -83,10 +90,10 @@ $(document).ready(function() {
     const errMessage = $(this).parent().find(".error-msg");
     const errText = $(this).parent().find(".error-txt")[0];
     if (!submission[0].text.value) {
-      $(errMessage).show("fast");
+      $(errMessage).show();
       errText.innerText = "Your Tweet is empty!";
     } else if (submission[0].text.value.length > 140) {
-      $(errMessage).show("fast");
+      $(errMessage).show();
       errText.innerText = "Your Tweet is to Long!";
     } else {
       $(errMessage).hide("fast");
@@ -98,7 +105,6 @@ $(document).ready(function() {
       });
       $(textBox).attr("rows", 1);
       $(textBox).val("");
-      $(newTweet).hide("slow");
     }
   });
 
@@ -112,7 +118,6 @@ $(document).ready(function() {
     $(window).scrollTop(0);
     $(backToTop).hide();
     $(newTweet).show();
-
   });
 
 
@@ -123,7 +128,7 @@ $(document).ready(function() {
       $(toggle).hide();
     } else {
       $(backToTop).hide();
-      $(toggle).show()
+      $(toggle).show();
     }
   });
 });
